d0c7bc43ac569a870533414a33415acdd21f307c
hokada <hokada@00c15b19-8571-4186-b24b-f7764bea4836>
2009-12-09 05:33:03 +0000

    Added tests
    git-svn-id: https://curlorb.svn.sourceforge.net/svnroot/curlorb@20 00c15b19-8571-4186-b24b-f7764bea4836

---
+++ trunk/orb-server/test/tests6/ConcurrentService.java
@@ -0,0 +1,20 @@
+package tests6;
+
+import org.springframework.stereotype.Service;
+
+import com.curl.io.serialize.types.annotation.NotNull;
+import com.curlap.orb.security.RemoteService;
+
+@Service
+@RemoteService
+public class ConcurrentService {
+
+	@NotNull
+	public String[] search(int from, int to) {
+		String[] array = new String[to - from];
+		for (int i = from; i < to; i++) {
+			array[i - from] = "concurrent-tests " + i;
+		}
+		return array;
+	}
+}
