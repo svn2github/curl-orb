e2804bd2e22836a65fbaa684e353fd89c5fefb8b
ysugahara <ysugahara@00c15b19-8571-4186-b24b-f7764bea4836>
2010-08-19 02:05:07 +0000

    added comments.
    git-svn-id: https://curlorb.svn.sourceforge.net/svnroot/curlorb@57 00c15b19-8571-4186-b24b-f7764bea4836

--- trunk/orb-plugin/src/com/curlap/orb/plugin/common/JavaElementSearcher.java
+++ trunk/orb-plugin/src/com/curlap/orb/plugin/common/JavaElementSearcher.java
@@ -18,23 +18,32 @@ import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.core.search.TypeNameMatch;
 import org.eclipse.jdt.core.search.TypeNameMatchRequestor;
 
+/**
+ * Searcher to search java elements.
+ * 
+ * @author Yohsuke Sugahara
+ * @since 0.8
+ */
 public class JavaElementSearcher {
 	
 	private IJavaSearchScope scope;
 	private Set<String> packageNamesForMask;
 	private String currentPackageName;
 	
+	// constructor
 	public JavaElementSearcher(ICompilationUnit target) throws JavaModelException {
 		scope = createScope(target);
 		packageNamesForMask = createPackageNameMask(target);
 	
 		// get current package name.
-		//TODO: handle the case where the package declaration doesn't exist.
+		// TODO handle the case where the package declaration doesn't exist.
 		IPackageDeclaration[] pkgDecs = target.getPackageDeclarations();
 		for (IPackageDeclaration pkgDec :  pkgDecs)
 			currentPackageName = pkgDec.getElementName();
 	}
 	
+	// extracts package name from full qualified name.
+	//  e.g. com.curlap.orb.Foo -> com.curlap.orb
 	private String extractPackageName(String elementName) {
 		int lastIndex = elementName.lastIndexOf('.');
 		if (lastIndex != -1)
@@ -43,6 +52,8 @@ public class JavaElementSearcher {
 		return elementName; 
 	}
 	
+	// creates scope for searching given IJavaElement.
+	// the default is the source folder of target's project.  
 	protected IJavaSearchScope createScope(ICompilationUnit target) throws JavaModelException {
 		IJavaElement[] elements = { target.getJavaProject() };
 		return SearchEngine.createJavaSearchScope(
@@ -50,6 +61,8 @@ public class JavaElementSearcher {
 				IJavaSearchScope.SOURCES);
 	}
 
+	// retains imported packages' name in the target ICompilationUnit.
+	// this is because we may filter found items with it later.
 	protected Set<String> createPackageNameMask(ICompilationUnit target) throws JavaModelException {
 		Set<String> mask = new HashSet<String>();
 		//TODO: move to constants.
@@ -63,10 +76,21 @@ public class JavaElementSearcher {
 		return mask;
 	}
 	
+	// filters out items that aren't imported in target from found items.
 	protected TypeNameMatch maskPackageNames(List<TypeNameMatch> foundMatches) {
 		List<TypeNameMatch> candidates = new ArrayList<TypeNameMatch>();
 		TypeNameMatch currentPackage = null;
 
+		// picking priority:
+		//  Consider 3 Hoge classes exist in:
+		//    com.curlap.a.Hoge
+		//    com.curlap.b.Hoge
+		//    com.curlap.c.Hoge
+		//  And current target file is in com.curlap.a with using Hoge class in it.
+		//  We pick Hoge's package in order of priority blow:
+		//    1. import with the class name  e.g.) com.curlap.b.Hoge
+		//    2. current package  e.g.) com.curlap.a
+		//    3. import with *  e.g.) com.curlap.b.*
 		for (TypeNameMatch match : foundMatches) {
 			String matchPkgName = match.getPackageName();
 			if (currentPackageName.equals(matchPkgName))
@@ -87,6 +111,8 @@ public class JavaElementSearcher {
 			return null;
 	}
 	
+	// searches for given class name.
+	// return found items without any filtering.
 	public List<TypeNameMatch> searchRawClassInfo(String className) throws JavaModelException {
 		final List<TypeNameMatch>results = new ArrayList<TypeNameMatch>();
 		TypeNameMatchRequestor requestor = new TypeNameMatchRequestor() {
@@ -108,16 +134,22 @@ public class JavaElementSearcher {
 		return results;
 	}
 
+	// searches for given class name.
+	// return value is filtered by imported packages' name.
 	public TypeNameMatch searchClassInfo(String className) throws JavaModelException {
 		return maskPackageNames(searchRawClassInfo(className));
 	}
 
+	// searches for given jdt's signature styled class name. 
 	public TypeNameMatch searchClassInfoWithSignature(String classNameInSignature)
 	throws JavaModelException, IllegalArgumentException {
 		String className = Signature.toString(classNameInSignature);
 		return searchClassInfo(className);
 	}
 	
+	// searches for given class name.
+	// return found items without any filtering.
+	// TODO change the way to search multiple classes.
 	public List<TypeNameMatch> searchRawClassesInfo(String ... classNames) throws JavaModelException {
 		// name
 		char[][] charClassNames = new char[classNames.length][];
