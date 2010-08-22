0023325da318892a56b7a58b70b6f9b887f4453a
ysugahara <ysugahara@00c15b19-8571-4186-b24b-f7764bea4836>
2010-08-22 14:17:10 +0000

    Added javadoc parser. it parses just detail, @param and @return for now.
    git-svn-id: https://curlorb.svn.sourceforge.net/svnroot/curlorb@70 00c15b19-8571-4186-b24b-f7764bea4836

--- trunk/orb-plugin/src/com/curlap/orb/plugin/common/ASTJavadocVisitor.java
+++ trunk/orb-plugin/src/com/curlap/orb/plugin/common/ASTJavadocVisitor.java
@@ -1,72 +1,72 @@
 package com.curlap.orb.plugin.common;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Javadoc;
+import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TagElement;
+import org.eclipse.jdt.core.dom.TextElement;
 
 public class ASTJavadocVisitor extends ASTVisitor {
 
-	private JavadocContent javaDoc;
+	private JavadocContent javadocContent;
 	
-	public JavadocContent getJavadoc() {
-		return javaDoc;
+	public JavadocContent getJavadocContent() {
+		return javadocContent;
 	}
 	
 	public boolean visit(Javadoc node) {
 		JavadocContent content = new JavadocContent();
-		for (Object tagObj : node.tags()) {
-//			TagElement tag = (TagElement) tagObj;
-//			String tagName = tag.getTagName();
-//			String tagContent = tag.toString();
-//			System.out.println(tagName + ":" + tagContent);
-//			if (tagName == null)
-//				content.purpose = tagContent;
-//			else if (tagName.equals("@param"))
-//				content.params = tagContent;
-//			else if (tagName.equals("@return"))
-//				content.returnValue = tagContent;
-//			
-//			for (Object fragment : tag.fragments()) {
-//				System.out.println(fragment.getClass().getName() + ":" + fragment);
-//			}
-//			System.out.println("=============================");
+		for (Object tag : node.tags()) {
+			TagElement element = (TagElement) tag;
+			String tagName = element.getTagName();
+
+			// parse tag element depending on its name
+			if (tagName == null)
+				content.setPurpose(parseToText(element));
+			else if (tagName.equals("@param"))
+				content.getParams().add(parseToParam(element));
+			else if (tagName.equals("@return"))
+				content.setReturnValue(parseToText(element));
 		}
-		javaDoc = content;
+		javadocContent = content;
 		return super.visit(node);
 	}
 	
-	
-	public class JavadocContent {
-
-		private String purpose;
-		private String author;
-		private Map<String, String> params;
-		private String returnValue;
-		
-		public JavadocContent() {
-			purpose = "";
-			author = "";
-			params = new LinkedHashMap<String, String>();
-			returnValue = "";
-		}
-		
-		public String getPurpose() {
-			return purpose;
-		}
-
-		public String getAuthor() {
-			return author;
-		}
-
-		public Map<String, String> getParams() {
-			return params;
-		}
-
-		public String getReturnValue() {
-			return returnValue;
-		}
+	private String parseToText(TagElement element) {
+		StringBuilder strBuilder = new StringBuilder();
+		for (Object fragment : element.fragments())
+			if (fragment instanceof TextElement) {
+				String text = ((TextElement) fragment).getText();
+				strBuilder.append(text);
+			}
+		return trimSpaces(strBuilder.toString());
 	}
+	
+	private JavadocParamContent parseToParam(TagElement element) {
+		JavadocParamContent param = new JavadocParamContent();
+		for (Object fragment : element.fragments())					
+			if (fragment instanceof SimpleName) {
+				String name = ((SimpleName) fragment).getIdentifier();
+				param.setName(trimSpaces(name));
+			} else if (fragment instanceof TextElement) {
+				String text = ((TextElement) fragment).getText();
+				param.setText(trimSpaces(text));
+			}
+		return param;
+	}
+	
+	private String trimSpaces(String str) {
+		final int count = str.length();
+	    int len = count;
+	    int st = 0;
+	    char[] val = str.toCharArray();
+
+	    while (st < len && (val[st] <= ' ' || val[st] == '@')) {
+	        st++;
+	    }
+	    while (st < len && (val[len - 1] <= ' ' || val[st] == '@')) {
+	        len--;
+	    }
+	    return (st > 0 || len < count) ? str.substring(st, len) : str;
+	}	
 }
--- trunk/orb-plugin/src/com/curlap/orb/plugin/common/JavaElementAnalyzer.java
+++ trunk/orb-plugin/src/com/curlap/orb/plugin/common/JavaElementAnalyzer.java
@@ -8,33 +8,14 @@ import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
-import com.curlap.orb.plugin.common.ASTJavadocVisitor.JavadocContent;
-
 public class JavaElementAnalyzer {
 
 	public JavaElementAnalyzer() {
+		
 	}
-	
-//	public void getJavaDoc(ISourceRange range) {
-//		ASTParser parser = ASTParser.newParser(AST.JLS3);
-//		int offset = range.getOffset();
-//		int length = range.getLength();
-//		System.out.println("offset:" + offset + " length:" + length);
-//		parser.setSource(element);
-//		parser.setSourceRange(offset, length);
-//		parser.setKind(ASTParser.K_CLASS_BODY_DECLARATIONS);
-////		CompilationUnit unit = (CompilationUnit) parser.createAST(null);
-//		TypeDeclaration unit = (TypeDeclaration) parser.createAST(null);
-//		unit.accept(new ASTVisitor() {
-//			public boolean visit(Javadoc node) {
-//				System.out.println(node);
-//				return super.visit(node);
-//			}
-//		});
-//	}
-	
+		
 	public JavadocContent getJavaDoc(IMember member) throws JavaModelException {
-		// define target
+		// target
 		ICompilationUnit source = member.getCompilationUnit();
 		ISourceRange range = member.getSourceRange();
 		int offset = range.getOffset();
@@ -45,15 +26,11 @@ public class JavaElementAnalyzer {
 		parser.setSource(source);
 		parser.setSourceRange(offset, length);
 		parser.setKind(ASTParser.K_CLASS_BODY_DECLARATIONS);
-		TypeDeclaration unit = (TypeDeclaration) parser.createAST(null);
 		
-//		Javadoc javaDoc = unit.getJavadoc();
-//		ChildPropertyDescriptor desc = unit.getJavadocProperty();
-//		System.out.println(desc.getChildType());
-		
+		// parsing
+		TypeDeclaration unit = (TypeDeclaration) parser.createAST(null);		
 		ASTJavadocVisitor visitor = new ASTJavadocVisitor();
 		unit.accept(visitor);
-		return visitor.getJavadoc();
+		return visitor.getJavadocContent();
 	}
 }
-
